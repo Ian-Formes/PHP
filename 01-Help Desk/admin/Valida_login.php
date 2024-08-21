a8bdfc8210768de2021be7111cc923178a5f3fc8
Ian Formes <ianformes.dev@gmail.com>
2024-08-21 14:18:01 -0300

    Atualizado

--- 01-Help Desk/admin/Valida_login.php
+++ 01-Help Desk/admin/Valida_login.php
@@ -20,11 +20,17 @@ session_start();
 
 $usuarios_autenticado = false;
 $usuarios_id = null;
+$usuario_perfil_id = null;
+
+$perfis = array(
+    1 => 'Administrativo',
+    2 => 'Usuário'
+);
 
 $usuarios_app = array(
-    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' =>'1234'),
-    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' =>'abcd'),
-    array('id' => 3, 'email' => 'ian@formes.com.br', 'senha' =>'1234')
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' =>'1234', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' =>'abcd', 'perfil_id' => 1),
+    array('id' => 3, 'email' => 'ian@formes.com.br', 'senha' =>'1234', 'perfil_id' => 2)
 );
 
 
@@ -32,6 +38,7 @@ foreach($usuarios_app as $user){
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ){
         $usuarios_autenticado = true;
         $usuarios_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
     }
 };
 
@@ -39,6 +46,7 @@ if($usuarios_autenticado){
    // echo 'usuario autenticado com sucesso';
     $_SESSION['autenticado'] = 'SIM';
     $_SESSION['id'] = $usuarios_id;
+    $_SESSION['perfil_id'] = $usuario_perfil_id;
     header('Location:home.php');
     
 }else{
