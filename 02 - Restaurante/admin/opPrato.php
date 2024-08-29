8c364b4af29a4a65d80c1aec893bd4f81e8cdfbf
Ian Formes <ianformes.dev@gmail.com>
2024-08-29 15:14:29 -0300

    finalizado

--- 02 - Restaurante/admin/opPrato.php
+++ 02 - Restaurante/admin/opPrato.php
@@ -35,7 +35,7 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'excluir') {
     $id = $_GET['id'];
     $foto = $_GET['foto'];
 
-    $del = $pdo->prepare("DELETE FROM pratos WHERE idprato = ?");
+    $del = $pdo->prepare("DELETE FROM pratos WHERE idpratos = ?");
     $del->bindValue(1, $id);
     $del->execute();
 
@@ -50,13 +50,15 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
 
     $id = $_GET['id'];
     $fotodb = $_GET['foto'];
+    $idcardapio = $_POST['txt_cardapio'];
 
     //Testar
     if ($_FILES['file_foto']['size'] == 0) {
         //echo 'Sem foto';
-        $edit = $pdo->prepare("UPDATE pratos SET prato = ? WHERE idprato = ?");
+        $edit = $pdo->prepare("UPDATE pratos SET prato = ?, idcardapio = ? WHERE idpratos = ?");
         $edit->bindValue(1, $prato);
-        $edit->bindValue(2, $id);
+        $edit->bindValue(2, $idcardapio);
+        $edit->bindValue(3, $id);
         $edit->execute();
 
         header("Location: pgPrato.php");
@@ -65,10 +67,11 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'editar') {
         unlink('img/' . $fotodb);
 
         if (move_uploaded_file($foto_temp, $destino)) {
-            $edit = $pdo->prepare("UPDATE pratos SET prato = ?, foto = ? WHERE idprato = ?");
+            $edit = $pdo->prepare("UPDATE pratos SET prato = ?, idcardapio = ?, foto = ? WHERE idpratos = ?");
             $edit->bindValue(1, $prato);
-            $edit->bindValue(2, $foto);
-            $edit->bindValue(3, $id);
+            $edit->bindValue(2, $idcardapio);
+            $edit->bindValue(3, $foto);
+            $edit->bindValue(4, $id);
             $edit->execute();
     
             header("Location: pgPrato.php");
--- 02 - Restaurante/admin/pgPrato.php
+++ 02 - Restaurante/admin/pgPrato.php
@@ -42,14 +42,15 @@ require_once 'menu.php';
                                     placeholder="Digite o nome do Prato">
                             </div>
                             <div class="mb-3">
-                            <label class="form-label">Selecione o Cardápio</label>
+                                <label class="form-label">Selecione o Cardápio</label>
                                 <select class="form-select" name="txt_cardapio">
-                                    <?php 
-                                      $sql = $pdo->query("SELECT * FROM cardapios ORDER BY cardapio");
+                                    <?php
+                                    $sql = $pdo->query("SELECT * FROM cardapios ORDER BY cardapio");
 
-                                      while ($linha = $sql->fetch(PDO::FETCH_ASSOC)) {                        
-                                    ?>
-                                    <option value="<?php echo $linha['idcardapio']?>"> <?php echo $linha['cardapio']?> </option>
+                                    while ($linha = $sql->fetch(PDO::FETCH_ASSOC)) {
+                                        ?>
+                                        <option value="<?php echo $linha['idcardapio'] ?>"> <?php echo $linha['cardapio'] ?>
+                                        </option>
 
                                     <?php } ?>
                                 </select>
@@ -81,9 +82,10 @@ require_once 'menu.php';
 
                 <?php
                 $lista = $pdo->query(
-                "SELECT p.idpratos, p.prato, p.foto, c.cardapio, c.idcardapio
+                    "SELECT p.idpratos, p.prato, p.foto, c.cardapio, c.idcardapio
                     FROM pratos p INNER JOIN cardapios c
-                    ON p.idcardapio = c.idcardapio");
+                    ON p.idcardapio = c.idcardapio"
+                );
 
                 while ($linha = $lista->fetch(PDO::FETCH_ASSOC)) {
                     ?>
@@ -108,7 +110,7 @@ require_once 'menu.php';
                     </tr>
 
                     <!-- Modal Excluir - Início -->
-                    <div class="modal fade" id="modalExcluir<?php echo $linha['idprato'] ?>" tabindex="-1"
+                    <div class="modal fade" id="modalExcluir<?php echo $linha['idpratos'] ?>" tabindex="-1"
                         aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
@@ -118,7 +120,7 @@ require_once 'menu.php';
                                         aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <a href="opPrato.php?acao=excluir&id=<?php echo $linha['idprato'] ?>&foto=<?php echo $linha['foto'] ?>"
+                                    <a href="opPrato.php?acao=excluir&id=<?php echo $linha['idpratos'] ?>&foto=<?php echo $linha['foto'] ?>"
                                         class="btn btn-primary">Sim</a>
                                     <button class="btn btn-danger" data-bs-dismiss="modal">Não</button>
                                 </div>
@@ -128,7 +130,7 @@ require_once 'menu.php';
                     <!-- Modal Excluir - Fim -->
 
                     <!-- Modal Editar - Início -->
-                    <div class="modal fade" id="modalEditar<?php echo $linha['idprato'] ?>" tabindex="-1"
+                    <div class="modal fade" id="modalEditar<?php echo $linha['idpratos'] ?>" tabindex="-1"
                         aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
@@ -140,12 +142,31 @@ require_once 'menu.php';
                                 <div class="modal-body">
 
                                     <form
-                                        action="opPrato.php?acao=editar&id=<?php echo $linha['idprato'] ?>&foto=<?php echo $linha['foto'] ?>"
+                                        action="opPrato.php?acao=editar&id=<?php echo $linha['idpratos'] ?>&foto=<?php echo $linha['foto'] ?>"
                                         method="post" enctype="multipart/form-data">
                                         <div class="mb-3">
                                             <label class="form-label">Prato</label>
                                             <input type="text" class="form-control" name="txt_prato"
                                                 placeholder="Digite o nome do Prato" value="<?php echo $linha['prato'] ?>">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="" class="form-label"> Selecione o cardápio </label>
+                                            <select class="form-select" name="txt_cardapio">
+                                                <?php
+                                                $sql = $pdo->query("SELECT * FROM cardapios ORDER BY cardapio");
+
+                                                while ($linhac = $sql->fetch(PDO::FETCH_ASSOC)) {
+
+                                                    if($linha['idcardapio'] == $linhac['idcardapio']) {
+                                                    ?>
+                                                    <option value="<?php echo $linhac['idcardapio'] ?>">
+                                                        <?php echo $linhac['cardapio'] ?> </option>
+
+                                                <?php } else { ?>
+                                                    <option value="<?php echo $linhac['idcardapio'] ?>">
+                                                        <?php echo $linhac['cardapio'] ?> </option>
+                                                <?php } } ?>
+                                            </select>
                                         </div>
                                         <div class="mb-3">
                                             <label for="formFile" class="form-label">Foto</label>
